7279db4c814c4659d7fd14d0bb6b0e3f5c491b51
Valerio Bellizia <elendil@iMac-di-Valerio.local>
2015-03-05 21:44:53 +0100

    more meat to cook!

--- jacob.py
+++ jacob.py
@@ -4,38 +4,45 @@ import sublime, sublime_plugin
 
 class JacobGoToCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-
+        self.locations = []
         region = self.view.sel()[0]
         if region.begin() == region.end(): # is a point
             region = self.view.word(region)
 
         symbol = self.view.substr(region)
-        navigate_to(self.view, symbol)
+        
+        if len(symbol) == 0:
+            pass
+        else:
+            self.navigate_to(symbol)
 
-def where_is(sym, view):
-    locations = []
-    if len(view.window().project_file_name()) > 0:
-        locations = view.window().lookup_symbol_in_index(sym)
-    else:
-        locations = view.window().lookup_symbol_in_open_files(sym)
+    def navigate_to(self, sym):
+        self.locations = []
+        for loc in self.where_is(sym):
+            self.locations.append(str(loc))
 
-    return locations;
+        if len(self.locations) == 1:
+            self.view.window().open_file(get_file_with_position(self.locations[0]), sublime.ENCODED_POSITION)
+        elif len(self.locations) > 1:
+            self.view.window().show_quick_panel([self.locations], self.on_select)
+        else:
+            pass
+
+    def on_select(self, index):
+        self.view.window().open_file(get_file_with_position(self.locations[index]), sublime.ENCODED_POSITION)
+
+    def where_is(self, sym):
+        locations = []
+        
+        if self.view.window().project_file_name() is not None:
+            locations = self.view.window().lookup_symbol_in_index(sym)
+        else:
+            locations = self.view.window().lookup_symbol_in_open_files(sym)
+
+        return locations;
 
 def get_position_string(tuple):
     return ':' + str(tuple[0]) + ':' + str(tuple[1])
 
 def get_file_with_position(location):
     return location[0] + get_position_string(location[2])
-
-def navigate_to(view, sym):
-    locations = where_is(sym, view)
-
-    print(locations)
-
-    if len(locations) == 1:
-        view.window().open_file(get_file_with_position(locations[0]), sublime.ENCODED_POSITION)
-    elif len(locations) > 1:
-        # TODO
-        pass
-    else:
-        pass
