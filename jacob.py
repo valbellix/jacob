d9d46dac7e05192d6e31e9532360380d1f823556
Valerio Bellizia <valbellix@gmail.com>
2015-03-10 22:19:58 +0100

    more improvements, the panel now looks much better!

--- jacob.py
+++ jacob.py
@@ -1,5 +1,8 @@
-"""Just Another COde Browser"""
+"""Just Another COde Browser
+This is a simple Sublime Text 3 plugin that make code browsing easy
 
+https://github.com/valbellix/jacob
+"""
 import sublime, sublime_plugin, os
 
 def get_position_string(tuple):
@@ -7,6 +10,22 @@ def get_position_string(tuple):
 
 def get_file_with_position(location):
     return location[0] + get_position_string(location[2])
+
+class Location(object):
+    def __init__(self, loc):
+        self.file_name = os.path.basename(loc[0])
+        self.file_pos = get_file_with_position(loc)
+        self.pretty_name = os.path.normpath(self.file_name)
+
+    def to_display(self):
+        return [self.file_name, self.pretty_name]
+
+def get_location_list(locations):
+    locs = []
+    for i, loc in enumerate(locations):
+        locs.append(loc.to_display())
+
+    return locs;
 
 class JacobGoToCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -23,21 +42,18 @@ class JacobGoToCommand(sublime_plugin.TextCommand):
             self.navigate_to(symbol)
 
     def navigate_to(self, sym):
-        self.locations = []
         for loc in self.where_is(sym):
-            file_and_pos = get_file_with_position(loc)
-
-            self.locations.append([os.path.basename(file_and_pos), file_and_pos]);
+            self.locations.append(Location(loc))
 
         if len(self.locations) == 1:
-            self.go_to(self.locations[0][1])
+            self.go_to(self.locations[0])
         elif len(self.locations) > 1:
-            self.view.window().show_quick_panel(self.locations, self.on_select)
+            self.view.window().show_quick_panel(get_location_list(self.locations), self.on_select)
         else:
             pass
 
     def on_select(self, index):
-        self.go_to(self.locations[index][1])
+        self.go_to(self.locations[index])
 
     def where_is(self, sym):
         locations = []
@@ -50,4 +66,4 @@ class JacobGoToCommand(sublime_plugin.TextCommand):
         return locations;
 
     def go_to(self, loc):
-        self.view.window().open_file(loc, sublime.ENCODED_POSITION)
+        self.view.window().open_file(loc.file_pos, sublime.ENCODED_POSITION)
